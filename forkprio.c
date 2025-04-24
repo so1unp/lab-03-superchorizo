3d4bb813eda18f16a15dde97174b57f1e73160c9
Toroide10 <lauticrespo@outlook.es>
2025-04-24 17:02:33 -0300

    restart forkprio

--- forkprio.c
+++ forkprio.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/resource.h>
-#include <sys/time.h>
-#include <sys/times.h>
-#include <sys/wait.h>
+#include <sys/times.h>      // times()
 
 int busywork(void)
 {
     struct tms buf;
-    while (1)
-    {
+    for (;;) {
         times(&buf);
     }
-}
-
-void sigterm_handler(int sig)
-{
-    (void)sig; 
-
-    struct rusage usage;
-    getrusage(RUSAGE_SELF, &usage);
-    int prio = getpriority(PRIO_PROCESS, 0);
-    printf("Child %d (nice %2d):\t%ld.%06ld\n", getpid(), prio,
-           usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
-    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
@@ -36,73 +18,10 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    int n = atoi(argv[1]);
-    int secs = atoi(argv[2]);
-    int reduce = atoi(argv[3]);
+    int hijos = atoi(argv[1]);       //número de hijos
+    int segs = atoi(argv[2]);        //segundos
+    int prio = atoi(argv[3]);        //activar prioridad
 
-    if (n <= 0)
-    {
-        fprintf(stderr, "Número de procesos inválido.\n");
-        exit(EXIT_FAILURE);
-    }
 
-    pid_t *children = malloc(sizeof(pid_t) * (size_t)n);
-    if (children == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sigaction sa;
-    sa.sa_handler = sigterm_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            if (reduce)
-            {
-                int new_nice = i * 5;
-                setpriority(PRIO_PROCESS, 0, new_nice);
-            }
-            sigaction(SIGTERM, &sa, NULL);
-            busywork();
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            children[i] = pid;
-        }
-    }
-
-    if (secs > 0)
-    {
-        sleep((unsigned int)secs);
-        for (int i = 0; i < n; i++)
-        {
-            kill(children[i], SIGTERM);
-        }
-        for (int i = 0; i < n; i++)
-        {
-            waitpid(children[i], NULL, 0);
-        }
-    }
-    else
-    {
-        while (1)
-        {
-            pause();
-        }
-    }
-
-    free(children);
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
